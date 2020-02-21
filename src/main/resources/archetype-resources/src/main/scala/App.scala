8a4ab8cc7cb770e895e87ce88d9128823292798e
Antoine Amend <antoine.amend@gmail.com>
2020-02-21 21:09:45 +0000

    Initial commit

--- src/main/resources/archetype-resources/src/main/scala/App.scala
+++ src/main/resources/archetype-resources/src/main/scala/App.scala
@@ -1,17 +1,12 @@
 package ${package}
 
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.SparkSession
 
 object App {
 
-  def generateDataframe(spark: SparkSession): DataFrame = {
-    import spark.implicits._
-    List("${groupId}:${artifactId}:${version}").toDF("maven")
-  }
-
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder().appName("spark-test").getOrCreate()
-    generateDataframe(spark).show(1, truncate = false)
+    val spark = SparkSession.builder().appName("${artifactId}").getOrCreate()
+
   }
 
 }
--- src/main/resources/archetype-resources/src/test/scala/AppTest.scala
+++
@@ -1,9 +0,0 @@
-package ${package}
-
-import org.scalatest.Matchers
-
-class AppTest extends SparkSuite with Matchers {
-  sparkTest("Dataframe") { spark =>
-    App.generateDataframe(spark).show(1, truncate = false)
-  }
-}
